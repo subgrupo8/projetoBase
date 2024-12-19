06fc9cb236e0e8b47001010d246d9e69b1b6cb57
Hugo MS <hugomaranse@gmail.com>
2024-12-19 03:04:42 -0300

    correcao de titulo e pequeno avanco no codigo

---
+++ conversoes de velocidade.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+double kmh, ms, mph;
+
+int main ()
+{
+printf("Escolha o valor que deseja converter: \n1 - km/h \n2 - m/s\n3 - mph ");
+
+
+
+    return 0;
+}
+
