42843672a1d3171eee8e3b1ac889c72aef1cb482
sjonatan127 <sjonatan127@gmail.com>
2024-12-19 14:29:22 -0300

    conversores(tempo/armazenamento)

--- ConversorUnidades.c
+++ ConversorUnidades.c
@@ -1,10 +1,10 @@
 /******************************************************************************
  * Programa desenvolvido colaborativamente pelos residentes do Embarcatech
- * U3 - Linguagens e Ambientes de Programação
- * Tarefa: depuração e versionamento
+ * U3 - Linguagens e Ambientes de Programaï¿½ï¿½o
+ * Tarefa: depuraï¿½ï¿½o e versionamento
  * Residentes: 
  *  - Lucas Meira de Souza Leite
- *  -  
+ *  -  jonatan Ramos dos Santos
  *  -  
  *  -  
  *  -   
@@ -15,20 +15,24 @@
  *******************************************************************************/ 
 
 /*
+
     Programa para realizar conversor de unidades de comprimento, massa, volume, entre outros
 */
 
 #include <stdio.h>
-#include <stdlib.h> //Necessário para o system("cls")
-#include <locale.h> //Necessário para o setlocale
+#include <stdlib.h> //Necessï¿½rio para o system("cls")
+#include <locale.h> //Necessï¿½rio para o setlocale
+
+void converterUnidadesDeTempo(void);
+void converterUnidadeDeArmazenamento(void);
 
 int main() {
-    setlocale(LC_ALL, "Portuguese_Brazil"); //Necessário para exibição de palavras com acento
+    setlocale(LC_ALL, "Portuguese_Brazil"); //Necessï¿½rio para exibiï¿½ï¿½o de palavras com acento
     int escolha = 0;
     char continuar = 'N';
     do  
     {
-        system("cls"); //Limpa a tela a cada iteração       
+        system("cls"); //Limpa a tela a cada iteraï¿½ï¿½o       
         printf("********************************************\n");
         printf("***         Conversor de Unidades        ***\n");
         printf("********************************************\n");        
@@ -42,44 +46,175 @@ int main() {
         printf(" 8 - Unidades de tempo                      \n");        
         printf(" 9 - Unidades de armazenamento              \n");                                
         printf("********************************************\n");                
-        printf("Escolha uma opção:      ");
+        printf("Escolha uma opï¿½ï¿½o:      ");
         scanf("%d", &escolha);
 
         switch (escolha) {
             case 1:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 2:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 3:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 4:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 5:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 6:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 7:
-                //chamada da função
+                //chamada da funï¿½ï¿½o
                 break;
             case 8:
-                //chamada da função
+                converterUnidadesDeTempo();
                 break;
             case 9:
-                //chamada da função
+                converterUnidadeDeArmazenamento();
                 break;
             default:
-                printf("Opção inválida.\n");
+                printf("Opï¿½ï¿½o invï¿½lida.\n");
         }
-        printf("\nDeseja escolher outra opção? (S/N)");
-        scanf(" %c", &continuar); //O espaço antes de %c é necessário para ignorar espaços em branco e realmente ler a variável
+        printf("\nDeseja escolher outra opï¿½ï¿½o? (S/N)");
+        scanf(" %c", &continuar); //O espaï¿½o antes de %c ï¿½ necessï¿½rio para ignorar espaï¿½os em branco e realmente ler a variï¿½vel
     } while ((continuar == 'S') || (continuar == 's'));
 
     return 0;
     system("PAUSE");
+}
+
+void converterUnidadesDeTempo(void){
+    int escolha;
+    double valor;
+
+    printf("Conversor de unidades de tempo\n");
+    printf("Escolha uma unidade de entrada:\n");
+    printf("1 - Segundos\n");
+    printf("2 - Minutos\n");
+    printf("3 - Horas\n");
+    scanf("%d", &escolha);
+    double segundos, minutos, horas;
+
+    
+    if (escolha >= 1 && escolha <= 3) {
+        printf("Digite o valor a ser convertido: ");
+        scanf("%lf", &valor);
+        
+    } 
+    switch (escolha) {
+        case 1: // Converter de segundos para minutos/horas
+            segundos = valor;
+            minutos = valor / 60.0;
+            horas = valor / 3600.0;
+            printf("%.2f segundos equivalem a %.2f minutos ou %.2f horas.\n", segundos, minutos, horas);
+            break;
+
+        case 2: // Converter de minutos para segundos/ horas 
+            minutos = valor;
+            segundos = valor * 60.0;
+            horas = valor / 60.0;
+            printf("%.2f minutos equivalem a %.2f segundos ou %.2f horas.\n", minutos, segundos, horas);
+            break;
+
+        case 3: // Converter de horas pra segundos/ minuto
+            horas = valor;
+            segundos = valor * 3600.0;
+            minutos = valor * 60.0;
+            printf("%.2f horas equivalem a %.2f segundos ou %.2f minutos.\n", horas, segundos, minutos);
+            break;
+
+        default:
+            printf("OpÃ§Ã£o invÃ¡lida.\n");
+    }
+}
+
+void converterUnidadeDeArmazenamento(void)
+{
+    int escolha;
+    double valor;
+
+    printf("Conversor de bits, bytes, kylobytes,Megabytes, gigabytes, Terabytes\n");
+    printf("Escolha uma unidade de entrada:\n");
+    printf("1 - bit\n");
+    printf("2 - byte\n");
+    printf("3 - Megabyte\n");
+    printf("4 - gigabyte\n");
+    printf("5 - terabyte\n");
+    scanf("%d", &escolha);
+    double bits, bytes, kylobytes, megabyte, gigabyte, terabyte;
+
+    if (escolha >= 1 && escolha <= 5)
+    {
+        printf("Digite o valor a ser convertido: ");
+        scanf("%lf", &valor);
+    }
+    switch (escolha)
+    {
+    case 1: // Converter de bits
+        bits = valor;
+        bytes = valor / 8;
+        kylobytes = (valor / 8) / 1024;
+        megabyte = ((valor / 8) / 1024) / 1024;
+        gigabyte = (((valor / 8) / 1024) / 1024) / 1024;
+        terabyte = ((((valor / 8) / 1024) / 1024) / 1024) / 1024;
+        printf("%.2f bits valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+
+    case 2: // Converter bytes
+        bits = valor * 8;
+        bytes = valor;
+        kylobytes = valor / 1024;
+        megabyte = ((valor) / 1024) / 1024;
+        gigabyte = (((valor) / 1024) / 1024) / 1024;
+        terabyte = ((((valor) / 1024) / 1024) / 1024) / 1024;
+        printf("%.2f bytes valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+
+    case 3: // Converter de kilobytes
+        bits = valor * 1024 * 8;
+        bytes = valor * 1024;
+        kylobytes = valor;
+        megabyte = valor / 1024;
+        gigabyte = (valor / 1024) / 1024;
+        terabyte = (((valor) / 1024) / 1024) / 1024;
+        printf("%.2f bytes valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+
+    case 4: // Converter de megabytes
+        bits = valor * 1024 * 1024 * 8;
+        bytes = valor * 1024 * 1024;
+        kylobytes = valor * 1024;
+        megabyte = valor;
+        gigabyte = (valor / 1024);
+        terabyte = (((valor) / 1024) / 1024);
+        printf("%.2f bytes valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+
+    case 5: // Converter de gigabytes
+        bits = valor * 1024 * 1024 * 1024 * 8;
+        bytes = valor * 1024 * 1024 * 1024;
+        kylobytes = valor * 1024 * 1024;
+        megabyte = valor * 1024;
+        gigabyte = valor;
+        terabyte = ((valor) / 1024);
+        printf("%.2f bytes valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+
+    case 6: // Converter de terabytes
+        bits = valor * 1024 * 1024 * 1024 * 1024 * 8;
+        bytes = valor * 1024 * 1024 * 1024 * 1024;
+        kylobytes = valor * 1024 * 1024 * 1024;
+        megabyte = valor * 1024 * 1024;
+        gigabyte = valor * 1024;
+        terabyte = valor;
+        printf("%.2f bytes valem a %.2f bytes valem a %.2f kylobytes valem a %.2f megabytes valem a %.2f gigabyte valem a %2.f terabytes  .\n", bits, bytes, kylobytes, megabyte, gigabyte, terabyte);
+        break;
+    default:
+        printf("OpÃ§Ã£o invÃ¡lida.\n");
+    }
 }
